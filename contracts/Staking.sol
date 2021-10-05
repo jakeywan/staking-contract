686aad0aba246decedd820637172f646c20d07aa
Jake Allen <jake@hintsy.co>
2021-10-05 11:42:13 -0700

    Removed reward token (for simplicity) and added test token  to contract deployer.

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -1,11 +1,10 @@
-pragma solidity ^0.7.3;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Staking {
 
     IERC20 public stakingToken;
-    IERC20 public rewardsToken;
 
     uint256 private totalSupply;
     mapping(address => uint256) private balances;
@@ -14,11 +13,9 @@ contract Staking {
     event Widthdrawn(address indexed sender, uint256 amount);
 
     constructor(
-        address _stakingToken,
-        address _rewardsToken
+        address _stakingToken
     ) {
         stakingToken = IERC20(_stakingToken);
-        rewardsToken = IERC20(_rewardsToken);
     }
 
     function deposit(uint256 amount) external {
--- scripts/hardhatDeploy.js
+++ scripts/hardhatDeploy.js
@@ -1,7 +1,7 @@
 async function main() {
   // We get the contract to deploy
   const Staking = await ethers.getContractFactory('Staking');
-  const staking = await Staking.deploy();
+  const staking = await Staking.deploy('0x6810e029d24fa8a9ab73b6789a701621753b5b64');
 
   console.log('DEPLOYED TO', staking.address);
 }
