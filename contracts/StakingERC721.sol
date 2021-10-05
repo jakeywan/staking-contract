cae2ce3a13dda0914adf388527564bbae13f0866
Jake Allen <jake@hintsy.co>
2021-10-05 15:12:38 -0700

    Added ERC721 version, with support for onERC721Received.

---
+++ contracts/StakingERC721.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
+contract StakingERC721 is IERC721Receiver {
+
+    event Staked(address indexed sender);
+    event Withdrawn(address indexed sender);
+    
+    mapping (address => mapping (uint256 => address)) tokenToStakerMap;
+
+    constructor() {}
+
+    function stake(address nftAddress, uint256 tokenId) public {
+        tokenToStakerMap[nftAddress][tokenId] = msg.sender;
+        ERC721(nftAddress).safeTransferFrom(msg.sender, address(this), tokenId);
+
+        emit Staked(msg.sender);
+    }
+
+    function widthdraw(address nftAddress, uint256 tokenId) external {
+        address staker = tokenToStakerMap[nftAddress][tokenId];
+        require(staker == msg.sender, "You are not the OG owner");
+        ERC721(nftAddress).safeTransferFrom(address(this), msg.sender, tokenId);
+
+        emit Withdrawn(msg.sender);
+    }
+
+    // Auto-stake an NFT when a user simply transfers it to this contract
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes memory data
+    ) external override returns (bytes4) {
+        tokenToStakerMap[operator][tokenId] = from;
+        emit Staked(from);
+        return bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"));
+    }
+
+}
--- scripts/hardhatDeploy.js
+++ scripts/hardhatDeploy.js
@@ -1,7 +1,7 @@
 async function main() {
   // We get the contract to deploy
-  const Staking = await ethers.getContractFactory('Staking');
-  const staking = await Staking.deploy('0x6810e029d24fa8a9ab73b6789a701621753b5b64');
+  const Staking = await ethers.getContractFactory('StakingERC721');
+  const staking = await Staking.deploy();
 
   console.log('DEPLOYED TO', staking.address);
 }
